ae1b9073079d38fbd22aa85b58467dcd84968234
Melany Carleto <110179828+melanycarleto@users.noreply.github.com>
2023-03-28 13:02:34 +0000

    Main Configuração básica do backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const { server: api } = require("./src/api/index");
+
+app.use("/api", api );
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
