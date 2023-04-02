f83ce31ec2a6db590d2b0569c5b8aa5416a78e70
Yasmin Xavier <110179982+XavierYR@users.noreply.github.com>
2023-04-02 19:07:47 +0000

    feat database updted

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require ('cors');
+const app = express();
+const port = 8080
 const { server: api } = require("./src/api/index");
+const { routes } = require ("./src/routes/index");
 
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 app.use("/api", api );
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
